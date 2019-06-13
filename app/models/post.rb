0e0952a747dae953e54596a48fc5f8491f1db296
canikwe <c.anikwe@gmail.com>
2019-06-13 19:19:40 -0400

    add dependent destroy to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   #Associations
   belongs_to :user
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags, dependent: :destroy
 
   accepts_nested_attributes_for :post_tags, allow_destroy: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   #Associations
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags, dependent: :destroy
   
   #Validations
