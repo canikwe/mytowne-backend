66958a814a98e07d964a9f9e62fce75f6a51b68a
canikwe <c.anikwe@gmail.com>
2019-08-21 08:01:59 -0400

    destroy likes dependencies when post is deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags, dependent: :destroy
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :user_likes, through: :likes, source: :user
 
   accepts_nested_attributes_for :post_tags, allow_destroy: true
