1ecf3ede9c768c80e8f0d097fa37f9cb7c84e15d
canikwe <c.anikwe@gmail.com>
2019-08-20 22:26:55 -0400

    delete action for like and render updated post back to the frontend

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::LikesController < ApplicationController
+  
   def create
     like = Like.create(like_params)
-    render json: {post: PostSerializer.new(like.post), status: :success}, status: :success
-  end
-
-  def update
+    render json: {post: PostSerializer.new(like.post), status: :success}
   end
 
   def destroy
+    like = Like.find(params[:id]).destroy
+    post = like.post
+    render json: post
   end
 
   private
   def like_params
     params.require(:like).permit(:user_id, :post_id, :id)
   end
+
 end
