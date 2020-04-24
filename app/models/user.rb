6694048a449f463b902e66d8d7c410b41d9593e2
canikwe <c.anikwe@gmail.com>
2020-04-23 22:37:18 -0400

    add user follows and test model relationships working

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
 
   has_many :comments
 
+  has_many :following_users, class_name: 'UserFollow', foreign_key: :follower_id
+  has_many :follows, through: :following_users
+  has_many :followed_users, class_name: 'UserFollow', foreign_key: :followed_id
+  has_many :followers, through: :followed_users
+
   has_secure_password
 
   #Validations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_005901) do
+ActiveRecord::Schema.define(version: 2020_04_24_022450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 2020_04_21_005901) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
